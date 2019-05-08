1e76448d05990484ea6f87e24a7803702e5373e1
lrh <lirh@hyxt.com>
2019-05-08 22:26:26 +0800

    init

---
+++ maven-compiler-demo/src/main/java/com/hyxt/maven/MavenCompilerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hyxt.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenCompilerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenCompilerDemoApplication.class, args);
+	}
+
+}
