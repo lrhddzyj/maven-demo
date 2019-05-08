63ac7bef158b03a36c14ea39f9e64aeaadf9260a
lrh <lirh@hyxt.com>
2019-05-08 17:52:16 +0800

    init

---
+++ maven-enforcer-demo/src/main/java/com/hyxt/maven/MavenEnforcerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hyxt.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenEnforcerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenEnforcerDemoApplication.class, args);
+	}
+
+}
