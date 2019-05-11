c42a55960185ebf8d5cb37c0de415c4835fb8210
lrh <lirh@hyxt.com>
2019-05-11 14:59:33 +0800

    maven-resources-plugin

---
+++ maven-resources-profile-demo/src/main/java/com/hyxt/maven/MavenResourcesProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hyxt.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenResourcesProfileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenResourcesProfileDemoApplication.class, args);
+    }
+
+}
