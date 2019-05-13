2ebc2d5f9bc91e7bd519b55f06424e72d1f5ce07
lrh <lirh@hyxt.com>
2019-05-13 14:49:07 +0800

    maven-profile-plugin

---
+++ maven-profile-demo/src/main/java/com/hyxt/maven/MavenProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hyxt.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProfileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenProfileDemoApplication.class, args);
+    }
+
+}
