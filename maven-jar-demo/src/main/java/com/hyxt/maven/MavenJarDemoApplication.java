e72a1fd21f610be2aa998ebb2810acc4080376e6
lrh <lirh@hyxt.com>
2019-05-20 21:25:01 +0800

    maven-jar-plugin

---
+++ maven-jar-demo/src/main/java/com/hyxt/maven/MavenJarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hyxt.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenJarDemoApplication.class, args);
+    }
+
+}
